0d7ce4af748731b024b4ff3173875bd2193d54c6
zeynepsen <zszynpsen@gmail.com>
2020-10-23 01:14:15 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your functions here
+// map function
+
+function map(array, callback) {
+    let myArray = []
+    for (let i = 0; i < array.length; i++) {
+        myArray.push(callback(array[i]))
+    }
+    return myArray
+}
+
+// reduce function
+
+function reduce(array, callback, startingValue) {
+    if(startingValue != undefined) {
+        return array.reduce(callback) + startingValue
+    }
+    return array.reduce(callback)
+}
